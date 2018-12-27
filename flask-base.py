ae8b6d65517fbcd43137dcbc3a29d6ac3d776571
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-26 21:51:14 -0800

    Shell context processor

--- flask-base.py
+++ flask-base.py
@@ -1,3 +1,13 @@
-from app import create_app
+from app import create_app, db
+from app.models import User, Role
 
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Role': Role
+    }
