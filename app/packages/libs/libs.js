934958bc638a80c27e21dc5dea7ba04a3ad2cf3e
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-30 15:04:24 -0800

    Refactor for yarn workspaces, bootstrap/webpack setup

---
+++ app/packages/libs/libs.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+window.jQuery = $; window.$ = $;
+import './main.scss'
+import 'bootstrap'
---
+++ app/packages/libs/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    libs: './libs.js',
+  },
+  output: {
+    path: path.join(__dirname, '..', '..', 'static'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'}
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bs.bundle.css'
+    })
+  ]
+}
