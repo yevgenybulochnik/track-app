141892dc95a44baf33f072280e9f5641917ced5c
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2019-01-27 12:12:10 -0800

    Pass PWD to click build command

--- app/cli/__init__.py
+++ app/cli/__init__.py
@@ -1,3 +1,4 @@
+import os
 import click
 from app.cli.build import AssetBuilder
 
@@ -5,7 +6,11 @@ from app.cli.build import AssetBuilder
 def register(app):
     @app.cli.command()
     @click.option('--list-assets', '-a', is_flag=True)
-    def build(list_assets):
+    @click.argument('cwd', envvar='PWD')
+    def build(list_assets, cwd):
+        if not os.path.exists(cwd):
+            print('This command does not accept arguments')
+            return
         wab = AssetBuilder(app)
         if list_assets:
             wab.list_assets()
